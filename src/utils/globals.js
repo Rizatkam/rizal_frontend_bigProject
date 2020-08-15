5b79ddf2115891a4881c31776bfe77e902c881eb
Rizal Satria Kamal <rizalsatriakamal@gmail.com>
2020-08-15 11:19:02 +0700

    Decoding Token

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,3 +1,10 @@
-const token=window.localStorage.getItem("token");
-export const ENDPOINT = 'http://localhost:4000/';
-export const access_token = `${token}`;
+import jwt_decode from "jwt-decode";
+
+const token = window.localStorage.getItem("token");
+const decoded = jwt_decode(token);
+console.log(decoded, "ini data yang udah didecode");
+export const ENDPOINT = "http://localhost:4000/";
+export const access_token = token;
+export const role = decoded.role_id;
+export const uid = decoded.id;
+export const uname = decoded.name;
